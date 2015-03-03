4c40c5e6a10e82a3d26b2a7cb20ac3940ee019d8
mm-ocampo <ocampo.marg@gmail.com>
2015-03-03 10:11:04 +0800

    Moved computation to search controller

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -20,6 +20,24 @@ class Search extends CI_Controller
             echo "<br />";
         }
         */
+        $wordList = array();
+        /*[1] get total number of words */
+        $totalWords = 0;
+        $data['coursedesc'] = $this->search_model->retrieve_all_coursedesc()->result();
+        foreach ($data['coursedesc'] as $item) {
+            $totalWords += str_word_count($item->coursedesc);
+            $temp = str_word_count($item->coursedesc, 1);
+            /* save each word to wordlist */
+            foreach ($temp as $word) {
+                if(array_key_exists($word, $wordList))
+                    $wordList[$word] += 1;
+                else
+                    $wordList[$word] = 1;
+            }
+            /* end save */
+        }
+        $numberOfDistinctWords = count($wordList);
+        /* end [1] */
         $this->load->view('results', $data);
     }
 }
