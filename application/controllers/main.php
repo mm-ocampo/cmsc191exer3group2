31133902e3921ff7bf0fc664ec792c3d2fd9114f
mm-ocampo <ocampo.marg@gmail.com>
2015-03-03 09:42:16 +0800

    Computed total number of distinct words in all tuples

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -12,12 +12,23 @@ class Main extends CI_Controller {
 
 
 	public function index(){
+		$wordList = array();
 		/*[1] get total number of words */
 		$totalWords = 0;
 		$data['coursedesc'] = $this->search_model->retrieve_all_coursedesc()->result();
 		foreach ($data['coursedesc'] as $item) {
 			$totalWords += str_word_count($item->coursedesc);
+			$temp = str_word_count($item->coursedesc, 1);
+			/* save each word to wordlist */
+			foreach ($temp as $word) {
+				if(array_key_exists($word, $wordList))
+					$wordList[$word] += 1;
+				else
+					$wordList[$word] = 1;
+			}
+			/* end save */
 		}
+		$numberOfDistinctWords = count($wordList);
 		/* end [1] */
 		$this->load->view('home');
 	}
