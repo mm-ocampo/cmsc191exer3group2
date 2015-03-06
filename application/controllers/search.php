682ff060eefbd0146831014d8cbeedcb3dd0411b
mm-ocampo <ocampo.marg@gmail.com>
2015-03-06 13:15:38 +0800

    Results Sorted V1

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -52,8 +52,7 @@ class Search extends CI_Controller
             $item->weight = $temp;
             $rank[$item->coursecode] = $item->weight;
         }
-        echo var_dump(array_multisort($rank, SORT_DESC, $data['results1']));       
-        /*echo var_dump($data['results1']);*/
+        array_multisort($rank, SORT_DESC, $data['results1']);
         /* end rank all queries */
         $this->load->view('results', $data);
     }
